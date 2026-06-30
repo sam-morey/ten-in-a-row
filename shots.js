9072a9df0f92f5d99d92fb38509a7f8f7466be87
Sam <sam@altairo.ai>
2026-06-30 08:36:00 -0500

    Update shots.js

--- shots.js
+++ shots.js
@@ -52,6 +52,7 @@ const SHOTS = [
   { day: 39, date: "6/27/2026", result: "make", note: "" },
   { day: 40, date: "6/28/2026", result: "make", note: "" },
   { day: 41, date: "6/29/2026", result: "miss", note: "" },
+  { day: 42, date: "6/30/2026", result: "miss", note: "" },
   // ↑ add the next day below this line, e.g.:
   // { day: 42, date: "6/30/2026", result: "make", note: "" },
 ];
